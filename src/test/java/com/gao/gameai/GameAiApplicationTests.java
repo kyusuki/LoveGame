a2071d4f40349cb135c1d6da3255090a8419ed2c
kyusuki <3032498305@qq.com>
2026-05-17 10:06:00 +0800

    initial commit

---
+++ src/test/java/com/gao/gameai/GameAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gao.gameai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
